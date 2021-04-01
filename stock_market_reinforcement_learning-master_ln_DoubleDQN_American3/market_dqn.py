cdc1959f5ee68f08683a9b2a0798f38b95ebabde
Cynthia <Luyao.Z@hotmail.com>
2021-04-02 00:48:34 +0800

    Add files via upload

---
+++ stock_market_reinforcement_learning-master_ln_DoubleDQN_American3/market_dqn.py
@@ -0,0 +1,260 @@
+import numpy as np
+import logging # review
+
+from market_env2 import MarketEnv
+from market_model_builder import MarketModelBuilder
+import os
+import tensorflow as tf
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # ly 
+# config = tf.ConfigProto()  # ly
+# config.gpu_options.per_process_gpu_memory_fraction = 0.6  # ly
+# session = tf.Session(config=config)  # ly
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+config.log_device_placement = True
+session = tf.Session(config=config)
+
+def Lgr(message):
+    logging.basicConfig(
+        level = logging.INFO,
+        format = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s',
+        datefmt = '%Y-%m-%d %A %H:%M:%S',
+    ) # review
+    logger = logging.getLogger('market_dqn') # review
+    log_file = logging.FileHandler('GSPC.log') # review
+    logger.addHandler(log_file) # review
+    logger.info(message)
+    logger.removeHandler(log_file)
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class ExperienceReplay(object):
+    def __init__(self, max_memory=100, discount=.9):
+        self.max_memory = max_memory
+        self.memory = list()
+        self.discount = discount
+
+    def remember(self, states, game_over):
+        # memory[i] = [[state_t, action_t, reward_t, state_t+1], game_over?]
+        self.memory.append([states, game_over])
+        if len(self.memory) > self.max_memory:
+            del self.memory[0]
+
+    def get_batch(self, model, model1, batch_size=10):  #review: gradient of Q-learning
+        len_memory = len(self.memory)
+        num_actions = model.output_shape[-1]
+        inputs = []
+
+        dim = len(self.memory[0][0][0])
+        for i in xrange(dim):
+            inputs.append([])
+
+        targets = np.zeros((min(len_memory, batch_size), num_actions))
+        for i, idx in enumerate(np.random.randint(0, len_memory, size=min(len_memory, batch_size))):
+            state_t, action_t, reward_t, state_tp1 = self.memory[idx][0]
+            game_over = self.memory[idx][1]
+
+            for j in xrange(dim):
+                inputs[j].append(state_t[j][0])
+
+            # inputs.append(state_t)
+            # There should be no target values for actions not taken.
+            # Thou shalt not correct actions not taken #deep
+            targets[i] = model1.predict(state_tp1)[0]  #review: Q_sa
+            q = model.predict(state_tp1)
+            action = np.argmax(q[0])
+            Q_sa = targets[i][action] # review: targets_DoubleDQN_version
+            #Q_sa = np.max(model1.predict(state_tp1)[0])  #review: targets_version1
+            if game_over:  # if game_over is True
+                targets[i, action_t] = reward_t
+            else:
+                # reward_t + gamma * max_a' Q(s', a')
+                targets[i, action_t] = reward_t + self.discount * Q_sa
+
+        # inputs = np.array(inputs)
+        inputs = [np.array(inputs[i]) for i in xrange(dim)]
+
+        return inputs, targets  # inputs, Q_sa
+
+
+if __name__ == "__main__":
+    import sys
+    import codecs
+    from market_model_test import ModelEvaluate
+
+    codeListFilename = sys.argv[1]
+    modelFilename = sys.argv[2] if len(sys.argv) > 2 else None
+
+    codeMap = {}
+    f = codecs.open(codeListFilename, "r", "utf-8")
+
+    for line in f:
+        if line.strip() != "":
+            tokens = line.strip().split(",") if not "\t" in line else line.strip().split("\t")
+            codeMap[tokens[0]] = tokens[1]
+
+    f.close()
+
+    env = MarketEnv(dir_path="./data/", target_codes=codeMap.keys(), input_codes=[], start_date="2014-10-06",
+                    end_date="2018-05-31", sudden_death=-1.0)
+
+    # parameters
+    epsilon = .5  # exploration
+    min_epsilon = 0.1
+    epoch = 100 #review: original 100000
+    max_memory = 700 #review: original 5000
+    batch_size = 50
+    discount = 0.9
+    n_steps = 10
+
+    from keras.optimizers import SGD
+    # action-value function Q
+    model = MarketModelBuilder(modelFilename).getModel
+    sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True) # review: !!must exist even when loading weights
+    model.compile(loss='mse', optimizer='rmsprop') # review: !!must exist even when loading weights
+
+    #model.load_weights("dqntest.h5")  # load saved model # review: !!load unfinished model
+
+    # target action-value function Q~********************
+    model1 = MarketModelBuilder(modelFilename).getModel
+    sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)  # review: !!must exist evern when loading weights
+    model1.compile(loss='mse', optimizer='rmsprop')  # review: !!must exist even when loading weights
+    model1.set_weights(model.get_weights())
+
+    # Initialize experience replay object
+    exp_replay = ExperienceReplay(max_memory=max_memory, discount=discount)
+
+    # Train
+    win_cnt = 0
+    for e in range(epoch):
+        loss = 0.
+        env._reset()
+        game_over = False
+        change = False #change_tm1
+        change_tm1 = False #change_tm2
+        # get initial input
+        input_t, rightaction = env._reset() # review: add rightaction
+        cumReward = 0.0
+        lastreward = 0
+        number_total = 0 # review:
+        number_dl = 0 # review:
+        Rightnumber_total = 0 # review:
+        Rightnumber_dl = 0 # review:
+        accuracy_total = 0.0 # review:
+        accuracy_dl = 0.0 # review:
+        capital_long = 1
+        capital_short = 1
+        capitallist_long = []
+        capitallist_short = []
+        inv_return = 0.0
+        #TP = 0.0  # review: true long
+        #FP = 0.0  # review: false short
+        #TN = 0.0  # review: true short
+        #FN = 0.0  # review: false long
+        #ori_matrix = [[TP, TN],[FP, FN]] # review: original statistics matrix for F1_score
+
+        while not game_over: #review: game_over = self.done in market_env
+            input_tm1 = input_t
+            isRandom = False
+            number_total += 1 # review:
+
+            # get next action
+            if np.random.rand() <= epsilon:
+                action = np.random.randint(0, env.action_space.n, size=1)[0]
+                input_t, cumpolicyreward, game_over, change, info, rightaction, capitallist_long, capitallist_short = env._step(
+                    action)  # review: input_t = self.state in market_env
+
+                isRandom = True
+            else:
+                q = model.predict(input_tm1)
+                action = np.argmax(q[0])
+
+                # print "  ".join(["%s:%.2f" % (l, i) for l, i in zip(env.actions, q[0].tolist())])
+                if np.nan in q:
+                    print "OCCUR NaN!!!"
+                    exit()
+
+                input_t, cumpolicyreward, game_over, change, info, rightaction, capitallist_long, capitallist_short = env._step(
+                    action)  # review: input_t = self.state in market_env
+                number_dl += 1  # review:
+                if action == rightaction:
+                    Rightnumber_dl += 1 # review:
+                accuracy_dl = float(Rightnumber_dl) / float(number_dl)  # review:
+
+            # apply action, get rewards and new state
+            #input_t, reward, game_over, info, rightaction = env._step(action) #review: original: input_t = self.state in market_env
+            if change:#change_tm1:#change:#change_tm1:
+                reward = cumpolicyreward #cumpolicyreward_tm1
+            else:
+                reward = cumpolicyreward- lastreward
+            cumReward += reward
+
+            lastreward = cumpolicyreward
+            change_tm1 = change
+            if action == rightaction:
+                Rightnumber_total += 1 # review:
+                #ori_matrix[0][action] += 1 # review: compute the trueactions (right long and right short)
+            #else:
+                #ori_matrix[1][action] += 1 # review: compute the actions (long and short)
+
+            accuracy_total = float(Rightnumber_total)/float(number_total) # review:
+
+            print action, rightaction # review: !!!!test
+            print number_dl, Rightnumber_dl, number_total, Rightnumber_total # review: !!!! test
+
+            if env.actions[action] == "LONG" or env.actions[action] == "SHORT":
+                color = bcolors.FAIL if env.actions[action] == "LONG" else bcolors.OKBLUE
+                if isRandom:
+                    color = bcolors.WARNING if env.actions[action] == "LONG" else bcolors.OKGREEN
+                print "%s:\t%s\t%.2f\t%.2f\t" % (
+                info["dt"], color + env.actions[action] + bcolors.ENDC, cumReward, info["cum"]) + ("\t".join(
+                    ["%s:%.2f" % (l, i) for l, i in zip(env.actions, q[0].tolist())]) if isRandom == False else "") # review: original
+                #logger2.info("%s:\t%s\t%.4f\t%.4f\t" % (
+                #info["dt"], color + env.actions[action] + bcolors.ENDC, cumReward, info["cum"]) + ("\t".join(
+                    #["%s:%.2f" % (l, i) for l, i in zip(env.actions, q[0].tolist())]) if isRandom == False else "")) # review
+                #logger2.info("accuracy_dl: %.4f, accuracy_total: %.4f" % (accuracy_dl, accuracy_total )) # review: logger accuracy
+                print "accuracy_dl: %.4f, accuracy_total: %.4f" % (
+                accuracy_dl, accuracy_total)  # review: logger accuracy
+
+            # store experience
+            exp_replay.remember([input_tm1, action, reward, input_t], game_over)
+
+            # adapt model
+            inputs, targets = exp_replay.get_batch(model, model1, batch_size=batch_size)
+
+            loss += model.train_on_batch(inputs, targets)
+
+            if number_total % n_steps == 0:
+                model1.set_weights(model.get_weights())
+
+        capital_long = max(capitallist_long) if len(capitallist_long) != 0 else 0
+        capital_short = max(capitallist_short) if len(capitallist_short) != 0 else 0
+        modelevalue = ModelEvaluate(codeListFilename=codeListFilename, Numtest=1)
+        modelevalue._readfile()
+        modelevalue._loadmodel(model=model, model1=model1, load=False)
+        modelevalue._test()
+
+        if cumReward > 0 and game_over:
+            win_cnt += 1
+
+
+        #print("Epoch {:03d}/{} | Loss {:.4f} | Win count {} | Epsilon {:.4f}".format(e, epoch, loss, win_cnt, epsilon)) # review: orginal
+        Lgr("Epoch {:03d}/{} | Loss {:.4f} | Win count {} | Epsilon {:.4f}".format(e, epoch, loss, win_cnt, epsilon)) # review
+        Lgr(
+            "accuracy_dl: %.4f, accuracy_total: %.4f" % (accuracy_dl, accuracy_total))  # review: logger accuracy
+        Lgr("capital_long: %s, capital_short: %s" % (capital_long, capital_short))
+        # Save trained model weights and architecture, this will be used by the visualization code
+        model.save_weights("model.h5" if modelFilename == None else modelFilename, overwrite=True)
+        epsilon = max(min_epsilon, epsilon * 0.99)
+
+    Lgr("epsilon: %ep" %epsilon)
---
+++ stock_market_reinforcement_learning-master_ln_DoubleDQN_American3/market_model_builder.py
@@ -0,0 +1,139 @@
+from deeplearning_assistant.model_builder import AbstractModelBuilder
+
+class MarketPolicyGradientModelBuilder(AbstractModelBuilder):
+
+	def buildModel(self):
+		from keras.models import Model
+		from keras.layers import merge, Conv2D, MaxPooling2D, Input, Dense, Flatten, Dropout, Reshape, TimeDistributed, BatchNormalization, Merge
+		from keras.layers import concatenate
+		from keras.layers.advanced_activations import LeakyReLU
+
+		B = Input(shape=(3,))
+		b = Dense(5, activation = "relu")(B)
+
+		inputs = [B]
+		merges = [b]
+
+		for i in xrange(1):
+			S = Input(shape=[2, 60, 1])
+			inputs.append(S)
+
+			h = Conv2D(2048, (1, 3), padding = 'valid')(S)
+			h = LeakyReLU(0.001)(h)
+			h = Conv2D(2048, (1, 5), padding = 'valid')(S)
+			h = LeakyReLU(0.001)(h)
+			h = Conv2D(2048, (1, 10), padding = 'valid')(S)
+			h = LeakyReLU(0.001)(h)
+			h = Conv2D(2048, (1, 20), padding = 'valid')(S)
+			h = LeakyReLU(0.001)(h)
+			h = Conv2D(2048, (1, 40), padding = 'valid')(S)
+			h = LeakyReLU(0.001)(h)
+
+			h = Flatten()(h)
+			h = Dense(512)(h)
+			h = LeakyReLU(0.001)(h)
+			merges.append(h)
+
+			h = Conv2D(2048, (1, 60), padding = 'valid')(S)
+			h = LeakyReLU(0.001)(h)
+
+			h = Flatten()(h)
+			h = Dense(512)(h)
+			h = LeakyReLU(0.001)(h)
+			merges.append(h)
+
+		m = concatenate(merges, axis = -1)
+		m = Dense(1024)(m)
+		m = LeakyReLU(0.001)(m)
+		m = Dense(512)(m)
+		m = LeakyReLU(0.001)(m)
+		m = Dense(256)(m)
+		m = LeakyReLU(0.001)(m)
+		V = Dense(2, activation = 'softmax')(m)
+		model = Model(output = V, inputs = inputs)
+
+		return model
+
+class MarketModelBuilder(AbstractModelBuilder):
+	
+	def buildModel(self):
+		from keras.models import Model
+		from keras.layers import merge, Conv2D, MaxPooling2D, Input, Dense, Flatten, Dropout, Reshape, TimeDistributed, BatchNormalization#, Merge
+		from keras.layers import concatenate
+		from keras.layers.advanced_activations import LeakyReLU
+
+		dr_rate = 0.0
+
+		B = Input(shape=(3,)) # <tf.Tensor 'input_1:0' shape=(?, 3) dtype=float32> # review: because the len(self.state[0])=3
+		b = Dense(5, activation = "relu")(B) # <tf.Tensor 'dense_1/Relu:0' shape=(?, 5) dtype=float32>
+
+		inputs = [B]
+		merges = [b]
+		merges1 = []
+		hgt = 1
+
+		for i in xrange(1):
+			S = Input(shape=[4, 60, 1])
+			inputs.append(S) # [<tf.Tensor 'input_1:0' shape=(?, 3) dtype=float32>, <tf.Tensor 'input_2:0' shape=(?, 2, 60, 1) dtype=float32>]
+
+			h = Conv2D(64, (hgt, 3), padding="valid")(S) # <tf.Tensor 'conv2d_1/BiasAdd:0' shape=(?, 2, 58, 64) dtype=float32>
+			h = LeakyReLU(0.001)(h) # <tf.Tensor 'leaky_re_lu_1/LeakyRelu/Maximum:0' shape=(?, 2, 58, 64) dtype=float32>
+			h = MaxPooling2D(pool_size=(2,2), strides=None)(h) # review
+
+			h = Flatten()(h) # review
+			merges1.append(h) # review
+			h = Conv2D(128, (hgt, 5), padding="valid")(S)#(S) # <tf.Tensor 'conv2d_2/BiasAdd:0' shape=(?, 2, 56, 128) dtype=float32>
+			h = LeakyReLU(0.001)(h)
+			h = MaxPooling2D(pool_size=(2, 2), strides=None)(h)  # review
+
+			h = Flatten()(h) # review
+			merges1.append(h)  # review
+			h = Conv2D(256, (hgt, 10), padding="valid")(S)#(S) # review: original
+			h = LeakyReLU(0.001)(h) # review: original
+			h = MaxPooling2D(pool_size=(2, 2), strides=None)(h)  # review
+
+			h = Flatten()(h) # review
+			merges1.append(h)  # review
+			h = Conv2D(512, (hgt, 20), padding="valid")(S)#(S) # review: original
+			h = LeakyReLU(0.001)(h) # review: original
+			h = MaxPooling2D(pool_size=(2, 2), strides=None)(h)  # review
+
+			h = Flatten()(h) # review
+			merges1.append(h)  # review
+			h = Conv2D(1024, (hgt, 40), padding="valid")(S)#(S) # review: original
+			h = LeakyReLU(0.001)(h) # review: original
+			h = MaxPooling2D(pool_size=(2, 2), strides=None)(h)  # review
+
+			h = Flatten()(h)  # review
+			merges1.append(h)  # review
+			m1 = concatenate(merges1, axis=-1)  # review
+
+			#h = Flatten()(m1) #(h) # review: original
+			h = Dense(2048)(m1) #(h) # review: original
+			h = LeakyReLU(0.001)(h)
+			h = Dropout(dr_rate)(h)
+			merges.append(h)
+
+			h = Conv2D(2048, (hgt, 60), padding="valid")(S)
+			h = LeakyReLU(0.001)(h)
+
+			h = Flatten()(h)
+			h = Dense(4096)(h)
+			h = LeakyReLU(0.001)(h)
+			h = Dropout(dr_rate)(h)
+			merges.append(h)
+
+		m = concatenate(merges, axis=-1)
+		m = Dense(1024)(m)
+		m = LeakyReLU(0.001)(m)
+		m = Dropout(dr_rate)(m)
+		m = Dense(512)(m)
+		m = LeakyReLU(0.001)(m)
+		m = Dropout(dr_rate)(m)
+		m = Dense(256)(m)
+		m = LeakyReLU(0.001)(m)
+		m = Dropout(dr_rate)(m)
+		V = Dense(2, kernel_initializer="zero", activation="linear")(m)
+		model = Model(outputs=V, inputs=inputs)
+
+		return model
